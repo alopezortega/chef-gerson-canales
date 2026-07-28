7f61224933bdeae02ad328bda6e4d07cfa146e47
Alejandro López Ortega <73475450+alopezortega@users.noreply.github.com>
2026-07-28 21:16:30 +0200

    feat(quote-request): implement translated reactive form

--- src/app/pages/quote-request/quote-request.spec.ts
+++ src/app/pages/quote-request/quote-request.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuoteRequestComponent } from '../quote-request/quote-request';
+import { provideRouter } from '@angular/router';
+import { provideTranslateService } from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('QuoteRequest', () => {
   let component: QuoteRequestComponent;
@@ -9,6 +12,14 @@ describe('QuoteRequest', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [QuoteRequestComponent],
+      providers: [
+        provideRouter([]),
+        ReactiveFormsModule,
+        provideTranslateService({
+          lang: 'es',
+          fallbackLang: 'es',
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(QuoteRequestComponent);
