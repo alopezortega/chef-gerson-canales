0f5641d6667924854f9bdd2b27eb8ae8d3964a87
Alejandro López Ortega <73475450+alopezortega@users.noreply.github.com>
2026-08-20 10:40:59 +0200

    refactor(ui): extract reusable final CTA

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { TranslatePipe } from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { TranslatePipe } from "@ngx-translate/core";
+
+import { FinalCta } from "../../shared/components/final-cta/final-cta";
 
 @Component({
-  selector: 'app-home',
-  imports: [RouterLink, TranslatePipe],
-  templateUrl: './home.html',
-  styleUrl: './home.scss',
+  selector: "app-home",
+  imports: [RouterLink, TranslatePipe, FinalCta],
+  templateUrl: "./home.html",
+  styleUrl: "./home.scss",
 })
 export class HomeComponent {}
